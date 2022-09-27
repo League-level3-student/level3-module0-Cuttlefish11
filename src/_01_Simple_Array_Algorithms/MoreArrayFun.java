a40f560460c61d143bc5815bbcb373603f657ba7
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2022-09-26 18:25:22 -0700

    Tired

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -2,25 +2,62 @@ package _01_Simple_Array_Algorithms;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class MoreArrayFun {
     //1. Create a main method to test the other methods you write.
-
-
-
-    //2. Write a method that takes an array of Strings and prints all the Strings in the array.
-
+public static void main(String[] args) {
+	String[] chars =new String[5];
+	chars[0]="a1";
+	chars[1]="b2";
+	chars[2]="c3";
+	chars[3]="d4";
+	chars[4]="e5";
+	printer(chars);
+	backward(chars);
+	oddsAndEvens(chars);
+	randomness(chars);
+}
+	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
+    private static void printer(String[] chars) {
+	for (int i = 0; i < chars.length; i++) {
+		System.out.println(chars[i]);
+	}
+}
 
 
     //3. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in reverse order.
-
+    private static void backward(String[] chars) {
+	for (int i = chars.length-1; i >= 0; i--) {
+		System.out.println(chars[i]);
+	}
+}
 
 
     //4. Write a method that takes an array of Strings and prints every other String in the array.
-
+    private static void oddsAndEvens(String[] chars) {
+    	for (int i = 0; i < chars.length; i++) {
+    		if(i%2==0) {
+        		System.out.println(chars[i]);
+    		}
+    	}
+    }
 
     //5. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in a completely random order. Almost every run of the program should result in a different order.
-
+    private static void randomness(String[] chars) {
+	for (int i = 0; i < chars.length; i++) {
+		System.out.println(chars[i]);
+	}
+}
 
 }
+//String charA = JOptionPane.showInputDialog("enter a string of letters");
+//String charB = JOptionPane.showInputDialog("enter a string of letters, again");
+//String charC = JOptionPane.showInputDialog("enter a string of letters, again");
+//String[] charss =new String[3];
+//charss[0].equals(charA);
+//charss[1].equals(charB);
+//charss[2].equals(charC);
+//printer(charss);
--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,15 +4,22 @@ public class _00_1D_Array_Methods {
     //1. Complete the method so that it returns the sum of all
     //   of the integers in the array being passed in
     public static int sumIntArray(int[] values) {
-
-        return 0;
+    	int sumson = 0;
+for (int i = 0; i < values.length; i++) {
+	sumson +=values[i];
+}
+        return sumson;
     }
 
     //2. Complete the method so that it returns the average of all
     //   of the integers in the array being passed in
     public static double averageIntArray(int[] values) {
-
-        return 0;
+    	double averg = 0;
+    	for (int i = 0; i < values.length; i++) {
+    		averg +=values[i];
+    	}
+    	double z = averg/values.length;
+    	        return z;
     }
 
 
@@ -20,15 +27,25 @@ public class _00_1D_Array_Methods {
     //   array contains the value specified by the second parameter.
     //   It should otherwise return false.
     public static boolean containsIntValue(int[] array, int value) {
-
-        return false;
+boolean lean = false;
+for (int i = 0; i < array.length; i++) {
+	if (array[i] == value) {
+		lean = true;
+	}
+}
+        return lean;
     }
 
     //4. Complete the method so that it returns the index of the,
     //   first instance that the specified value occurs in the array.
     //   If the array does not contain the specified value, it should return -1.
     public static int getIndex(int[] arr, int value) {
-
-        return 0;
+    int first = -1;
+    	for (int i = 0; i < arr.length; i++) {
+    		if (arr[i] == value && first == -1) {
+    			first = i;
+    		}
+    	}
+        return first;
     }
 }
